0a545a5352b81f16a0d6487285c4180fb8998e85
Iaroslava Finta <iaroslava.sv7@gmail.com>
2024-01-31 20:50:36 -0600

    selected contact view addded with error

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -2,16 +2,17 @@ import React from "react";
 import { useState, useEffect } from "react";
 import ContactRow from "./ContactRow";
 
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
+// const dummyContacts = [
+//   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+//   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+//   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+// ];
 
-export default function ContactList() {
-    const [contacts, setContacts] = useState(dummyContacts);
-    
-    useEffect(() => {
+export default function ContactList({ setSelectedContactId }) {
+  
+  const [contacts, setContacts] = useState([]);
+      
+  useEffect(() => {
         async function fetchContacts() {
             try {
                 const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
@@ -24,8 +25,6 @@ export default function ContactList() {
         }
         fetchContacts()
     }, []);
-
-    console.log(contacts);
 
   return (
     <table>
